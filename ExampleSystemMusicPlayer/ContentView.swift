b7db374513bcb3a724dc20c5e5b53e341ec852d7
shi-n <shi-n_xyz@cloudsquare.jp>
2020-12-12 21:13:58 +0900

    Initial Commit

---
+++ ExampleSystemMusicPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleSystemMusicPlayer
+//
+//  Created by Shin Inaba on 2020/12/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleSystemMusicPlayer/ExampleSystemMusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleSystemMusicPlayerApp.swift
+//  ExampleSystemMusicPlayer
+//
+//  Created by Shin Inaba on 2020/12/12.
+//
+
+import SwiftUI
+
+@main
+struct ExampleSystemMusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
